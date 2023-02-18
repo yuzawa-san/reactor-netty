268dc9b68150b4e30b3f72552946d0f7b96a1a8e
James Yuzawa <jtyuzawa@gmail.com>
2023-02-18 17:48:39 -0500

    refactor UriEndpoint

--- reactor-netty-http/src/main/java/reactor/netty/http/client/FailedHttpClientRequest.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/FailedHttpClientRequest.java
@@ -23,7 +23,6 @@ import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
 import io.netty.handler.codec.http.cookie.Cookie;
 import reactor.netty.http.Cookies;
-import reactor.netty.http.HttpOperations;
 import reactor.util.context.Context;
 import reactor.util.context.ContextView;
 
@@ -45,9 +44,8 @@ final class FailedHttpClientRequest implements HttpClientRequest {
 	final HttpHeaders         headers;
 	final boolean             isWebsocket;
 	final HttpMethod          method;
-	final String              path;
 	final Duration            responseTimeout;
-	final String              uri;
+	final UriEndpoint         uriEndpoint;
 
 	FailedHttpClientRequest(ContextView contextView, HttpClientConfig c) {
 		this.contextView = contextView;
@@ -55,8 +53,7 @@ final class FailedHttpClientRequest implements HttpClientRequest {
 		this.headers = c.headers;
 		this.isWebsocket = c.websocketClientSpec != null;
 		this.method = c.method;
-		this.uri = c.uri == null ? c.uriStr : c.uri.toString();
-		this.path = this.uri != null ? HttpOperations.resolvePath(this.uri) : null;
+		this.uriEndpoint = UriEndpoint.fromConfig(c);
 		this.responseTimeout = c.responseTimeout;
 	}
 
@@ -89,7 +86,7 @@ final class FailedHttpClientRequest implements HttpClientRequest {
 
 	@Override
 	public String fullPath() {
-		return path;
+		return uriEndpoint.getPath();
 	}
 
 	@Override
@@ -144,12 +141,12 @@ final class FailedHttpClientRequest implements HttpClientRequest {
 
 	@Override
 	public String resourceUrl() {
-		return null;
+		return uriEndpoint.toExternalForm();
 	}
 
 	@Override
 	public String uri() {
-		return uri;
+		return uriEndpoint.getRawPathAndQuery();
 	}
 
 	@Override
--- reactor-netty-http/src/main/java/reactor/netty/http/client/UriEndpoint.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/UriEndpoint.java
@@ -17,70 +17,97 @@ package reactor.netty.http.client;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
-import io.netty.channel.unix.DomainSocketAddress;
+import io.netty.handler.codec.http.HttpUtil;
 import io.netty.util.NetUtil;
+import reactor.netty.transport.AddressUtils;
 
 final class UriEndpoint {
+	static final Pattern SCHEME_PATTERN = Pattern.compile("^\\w+://.*$");
+	static final String ROOT_PATH = "/";
+
+	final SocketAddress remoteAddress;
+	final URI uri;
 	final String scheme;
-	final String host;
-	final int port;
-	final Supplier<? extends SocketAddress> remoteAddress;
-	final String pathAndQuery;
 
-	UriEndpoint(String scheme, String host, int port, Supplier<? extends SocketAddress> remoteAddress, String pathAndQuery) {
-		this.host = host;
-		this.port = port;
-		this.scheme = Objects.requireNonNull(scheme, "scheme");
-		this.remoteAddress = Objects.requireNonNull(remoteAddress, "remoteAddressSupplier");
-		this.pathAndQuery = Objects.requireNonNull(pathAndQuery, "pathAndQuery");
+	private UriEndpoint(URI uri) {
+		this(uri, null);
 	}
 
-	boolean isWs() {
-		return HttpClient.WS_SCHEME.equals(scheme) || HttpClient.WSS_SCHEME.equals(scheme);
-	}
-
-	boolean isSecure() {
-		return isSecureScheme(scheme);
-	}
-
-	static boolean isSecureScheme(String scheme) {
-		return HttpClient.HTTPS_SCHEME.equals(scheme) || HttpClient.WSS_SCHEME.equals(scheme);
-	}
-
-	String getPathAndQuery() {
-		return pathAndQuery;
-	}
-
-	SocketAddress getRemoteAddress() {
-		return remoteAddress.get();
-	}
-
-	String toExternalForm() {
-		StringBuilder sb = new StringBuilder();
-		SocketAddress address = remoteAddress.get();
-		if (address instanceof DomainSocketAddress) {
-			sb.append(((DomainSocketAddress) address).path());
+	private UriEndpoint(URI uri, SocketAddress remoteAddress) {
+		this.uri = Objects.requireNonNull(uri, "uri");
+		if (!uri.isAbsolute()) {
+			throw new IllegalArgumentException("URI is not absolute: " + uri);
+		}
+		if (uri.getHost() == null) {
+			throw new IllegalArgumentException("URI is not absolute: " + uri);
+		}
+		this.scheme = uri.getScheme().toLowerCase();
+		if (remoteAddress == null) {
+			String host = cleanHostString(uri.getHost());
+			int port = uri.getPort() != -1 ? uri.getPort() : (isSecureScheme(scheme) ? 443 : 80);
+			this.remoteAddress = AddressUtils.createUnresolved(host, port);
 		}
 		else {
-			sb.append(scheme);
-			sb.append("://");
-			sb.append(address != null
-							  ? toSocketAddressStringWithoutDefaultPort(address, isSecure())
-							  : "localhost");
-			sb.append(pathAndQuery);
+			this.remoteAddress = remoteAddress;
 		}
-		return sb.toString();
+		System.out.println(uri + " " + this.remoteAddress + " " + toExternalForm());
 	}
 
-	static String toSocketAddressStringWithoutDefaultPort(SocketAddress address, boolean secure) {
+	static UriEndpoint fromConfig(HttpClientConfig config) {
+		return fromConfig(config.uri, config.uriStr, config.baseUrl, config.remoteAddress(), config.isSecure(), config.websocketClientSpec != null);
+	}
+
+	static UriEndpoint fromConfig(URI uri, String uriStr, String baseUrl, Supplier<? extends SocketAddress> remoteAddress, boolean secure, boolean ws) {
+		if (uri != null) {
+			return new UriEndpoint(uri);
+		}
+		uriStr = uriStr == null ? ROOT_PATH : uriStr;
+		if (baseUrl != null && uriStr.startsWith(ROOT_PATH)) {
+			if (baseUrl.endsWith(ROOT_PATH)) {
+				baseUrl = baseUrl.substring(0, baseUrl.length() - ROOT_PATH.length());
+			}
+			uriStr = baseUrl + uriStr;
+		}
+		if (uriStr.startsWith(ROOT_PATH)) {
+			// relative path
+			SocketAddress socketAddress = remoteAddress.get();
+			uriStr = resolveScheme(secure, ws) + "://" + toSocketAddressStringWithoutDefaultPort(socketAddress, secure) + uriStr;
+			return new UriEndpoint(URI.create(uriStr), socketAddress);
+		}
+		if (!SCHEME_PATTERN.matcher(uriStr).matches()) {
+			// support "example.com/path" case
+			uriStr = resolveScheme(secure, ws) + "://" + uriStr;
+		}
+		return new UriEndpoint(URI.create(uriStr));
+	}
+
+	UriEndpoint redirect(String to) {
+		try {
+			URI redirectUri = new URI(to);
+			if (redirectUri.isAbsolute()) {
+				// absolute path: treat as a brand new uri
+				return new UriEndpoint(redirectUri);
+			}
+			// relative path: reuse the remote address
+			return new UriEndpoint(uri.resolve(redirectUri), remoteAddress);
+		}
+		catch (URISyntaxException e) {
+			throw new IllegalArgumentException("Cannot resolve location header", e);
+		}
+	}
+
+	private static String toSocketAddressStringWithoutDefaultPort(SocketAddress address, boolean defaultSecure) {
 		if (!(address instanceof InetSocketAddress)) {
-			throw new IllegalStateException("Only support InetSocketAddress representation");
+			return "localhost";
 		}
 		String addressString = NetUtil.toSocketAddressString((InetSocketAddress) address);
-		if (secure) {
+		if (defaultSecure) {
 			if (addressString.endsWith(":443")) {
 				addressString = addressString.substring(0, addressString.length() - 4);
 			}
@@ -91,6 +118,87 @@ final class UriEndpoint {
 			}
 		}
 		return addressString;
+	}
+
+	private static String cleanHostString(String host) {
+		// remove brackets around IPv6 address in host name
+		if (host.charAt(0) == '[' && host.charAt(host.length() - 1) == ']') {
+			host = host.substring(1, host.length() - 1);
+		}
+		return host;
+	}
+
+	private static String resolveScheme(boolean secure, boolean ws) {
+		if (ws) {
+			return secure ? HttpClient.WSS_SCHEME : HttpClient.WS_SCHEME;
+		}
+		else {
+			return secure ? HttpClient.HTTPS_SCHEME : HttpClient.HTTP_SCHEME;
+		}
+	}
+
+	boolean isSecure() {
+		return isSecureScheme(scheme);
+	}
+
+	static boolean isSecureScheme(String scheme) {
+		return HttpClient.HTTPS_SCHEME.equals(scheme) || HttpClient.WSS_SCHEME.equals(scheme);
+	}
+
+	private void rawPathAndQuery(StringBuilder sb) {
+		String rawPath = uri.getRawPath();
+		if (rawPath == null || rawPath.isEmpty()) {
+			sb.append('/');
+		}
+		else {
+			sb.append(rawPath);
+		}
+		String rawQuery = uri.getRawQuery();
+		if (rawQuery != null) {
+			sb.append('?').append(rawQuery);
+		}
+	}
+
+	String getRawPathAndQuery() {
+		StringBuilder sb = new StringBuilder();
+		rawPathAndQuery(sb);
+		return sb.toString();
+	}
+
+	String getPath() {
+		String path = uri.getPath();
+		if (path == null || path.isEmpty()) {
+			return "/";
+		}
+		return path;
+	}
+
+	String getHostNameHeaderValue() {
+		if (remoteAddress instanceof InetSocketAddress) {
+			InetSocketAddress address = (InetSocketAddress) remoteAddress;
+			String host = HttpUtil.formatHostnameForHttp(address);
+			int port = address.getPort();
+			if (port != 80 && port != 443) {
+				host = host + ':' + port;
+			}
+			return host;
+		}
+		else {
+			return "localhost";
+		}
+	}
+
+	SocketAddress getRemoteAddress() {
+		return remoteAddress;
+	}
+
+	String toExternalForm() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(scheme);
+		sb.append("://");
+		sb.append(toSocketAddressStringWithoutDefaultPort(remoteAddress, isSecure()));
+		rawPathAndQuery(sb);
+		return sb.toString();
 	}
 
 	@Override
@@ -107,11 +215,11 @@ final class UriEndpoint {
 			return false;
 		}
 		UriEndpoint that = (UriEndpoint) o;
-		return getRemoteAddress().equals(that.getRemoteAddress());
+		return remoteAddress.equals(that.remoteAddress);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(getRemoteAddress());
+		return Objects.hash(remoteAddress);
 	}
 }
